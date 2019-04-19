3e87a0c1cdaec7fa4fffe62b45c0a8799019d451
Christian Zielke <zielke@fzi.de>
2019-04-19 14:38:53 +0200

    changed package name from r2 and build new apk

--- RuntimeExceptionApplication2/app/src/main/java/com/example/runtimeexceptionapplication2/MainActivity.java
+++ RuntimeExceptionApplication2/app/src/main/java/com/example/runtimeexceptionapplication2/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
-import com.example.runtimeexceptionapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
